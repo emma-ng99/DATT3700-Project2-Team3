160f9ef35b3696eb862215ae9a1f939f4b31828a
emma-ng99 <59710627+emma-ng99@users.noreply.github.com>
2022-03-29 15:51:37 -0400

    Unity Player1 script

---
+++ Player1.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using M2MqttUnity;
+
+/// <summary>
+/// Examples for the M2MQTT library (https://github.com/eclipse/paho.mqtt.m2mqtt),
+/// </summary>
+namespace ControlsUsingMQTTShiftr
+{
+    /// <summary>
+    /// Script for testing M2MQTT with a Unity UI
+    /// </summary>
+    public class Player1 : M2MqttUnityClient
+    {
+        [Tooltip("Set this to true to perform a testing cycle automatically on startup")]
+        public bool autoTest = true;
+
+        private List<string> eventMessages = new List<string>();
+       
+        Vector2 movement;
+        public float speed = 5f;
+        public Rigidbody2D rb;
+
+        public void TestPublish()
+        {
+            client.Publish("M2MQTT_Unity/connect1", System.Text.Encoding.UTF8.GetBytes(""), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            Debug.Log("Connected");
+        }
+
+        protected override void OnConnected()
+        {
+            base.OnConnected();
+
+            if (autoTest)
+            {
+                TestPublish();
+                
+            }
+        }
+
+        protected override void SubscribeTopics()
+        {
+            client.Subscribe(new string[] { "M2MQTT_Unity/connect1" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            client.Subscribe(new string[] { "M2MQTT_Unity/player1/W" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            client.Subscribe(new string[] { "M2MQTT_Unity/player1/S" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            client.Subscribe(new string[] { "M2MQTT_Unity/player1/A" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            client.Subscribe(new string[] { "M2MQTT_Unity/player1/D" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+
+        protected override void UnsubscribeTopics()
+        {
+            client.Unsubscribe(new string[] { "M2MQTT_Unity/test" });
+        }
+
+     
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected override void DecodeMessage(string topic, byte[] message)
+        {
+            string msg = System.Text.Encoding.UTF8.GetString(message);
+            
+
+            if (topic == "M2MQTT_Unity/player1/W")
+            {
+                Debug.Log("Received: W " + msg);
+                bool result;
+                double number;
+ 
+                result = double.TryParse(msg, out number);
+
+                if(result){
+                }
+
+                movement.y = (float)number/10;
+                Debug.Log("Player 1 Move up: " + movement.y);
+     
+            }
+
+            
+            if (topic == "M2MQTT_Unity/player1/S")
+            {
+                Debug.Log("Received: S " + msg);
+                bool result;
+                double number;
+ 
+                result = double.TryParse(msg, out number);
+
+                if(result){
+                }
+       
+                movement.y = -( (float)number/10);
+                Debug.Log("Player 1 Move down: " + movement.y);
+              
+            }
+
+           
+            if (topic == "M2MQTT_Unity/player1/A")
+            {
+                 Debug.Log("Received: A " + msg);
+                bool result;
+                double number;
+ 
+                result = double.TryParse(msg, out number);
+
+                if(result){
+                }
+           
+                movement.x = - ((float)number/10);
+                Debug.Log("Player 1 Move left: " + movement.x);
+               
+            }
+
+            
+            if (topic == "M2MQTT_Unity/player1/D")
+            {
+                Debug.Log("Received: D " + msg);
+                bool result;
+                double number;
+ 
+                result = double.TryParse(msg, out number);
+
+                if(result){
+                }
+               
+                movement.x = (float)number/10;
+                Debug.Log("Player 1 Move right: " + movement.x);
+                
+            }
+
+           
+        }
+
+      
+        protected override void Update()
+        {
+            base.Update(); // call ProcessMqttEvents()
+           
+            
+           
+        }
+
+        private void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        //transform.position += movement * Time.deltaTime * speed;
+    }
+
+        private void OnDestroy()
+        {
+            Disconnect();
+        }
+
+        private void OnValidate()
+        {
+            if (autoTest)
+            {
+                autoConnect = true;
+            }
+        }
+    }
+}
